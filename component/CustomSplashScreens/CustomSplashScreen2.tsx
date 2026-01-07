54f419c1d958bcba3179c3d03722586a7626ae9c
[joshua nduka] <[ndukajoshua6@gmail.com]>
2026-01-07 15:41:07 +0100

    j

--- component/CustomSplashScreens/CustomSplashScreen2.tsx
+++ component/CustomSplashScreens/CustomSplashScreen2.tsx
@@ -3,53 +3,60 @@ import React from 'react'
 
 export default function CustomSplashScreen2() {
   return (
-    <View style={styles.content}> 
-     <StatusBar barStyle='dark-content' backgroundColor='white' />
-    <Image style={styles.icon} source={require('../../assets/images/churchlogo.png')}/>
-      
-       <View style={styles.headerContainer}>
-              <Text style={styles.title}>OIKIA</Text>
-              <View>
-                <Text style={styles.title2}>CHRISTIAN</Text>
-                <Text style={styles.title2}>CENTRE</Text>
-              </View>
+    <View style={styles.container}>
+      {/* Status bar */}
+      <StatusBar barStyle='dark-content' backgroundColor='white' translucent={false} />
+
+      {/* Logo */}
+      <Image
+        style={styles.logo}
+        source={require('../../assets/images/churchlogo.png')}
+        resizeMode="contain"
+      />
+
+      {/* Header Text */}
+      <View style={styles.headerContainer}>
+        <Text style={styles.title}>OIKIA</Text>
+        <View>
+          <Text style={styles.title2}>CHRISTIAN</Text>
+          <Text style={styles.title2}>CENTRE</Text>
         </View>
-     
+      </View>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-    content:{
-        flex:1,
-        alignItems:'center',
-        justifyContent: 'center',
-        backgroundColor: 'white'
-    },
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingHorizontal: 20,
+  },
 
-    icon:{
-        width:174,
-        height:168
-    },
+  logo: {
+    width: 174,
+    height: 168,
+    marginBottom:25, 
+  },
 
   headerContainer: {
-    marginTop: 20,
-    alignSelf: 'center',
     flexDirection: 'row',
-    gap: 7,
     alignItems: 'center',
+    gap:1,
   },
 
   title: {
     fontSize: 40,
+    fontFamily: 'PoppinsSemiBold', 
     color: '#8C4616',
-    fontWeight: 'bold',
   },
 
   title2: {
+    fontFamily: 'PoppinsSemiBold',
     fontSize: 18,
     color: '#8C4616',
-    lineHeight: 17,
+    lineHeight: 22,
   },
-
 })
