f4b323870bb8f106758a436dc187a5cb3aeb89e8
[joshua nduka] <[ndukajoshua6@gmail.com]>
2026-01-07 15:27:53 +0100

    j

--- component/CustomSplashScreens/CustomSplashScreen1.tsx
+++ component/CustomSplashScreens/CustomSplashScreen1.tsx
@@ -1,18 +1,69 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React from "react";
+import { View, Text, Image, StyleSheet, StatusBar } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 
+// If you're using Poppins, make sure fonts are loaded before rendering this component
 export default function CustomSplashScreen1() {
   return (
-    <View style={styles.content}> 
-      <Text>CustomSplashScreen1</Text>
-    </View>
-  )
+    <>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <LinearGradient
+        colors={["#2B1F16", "#8C4616"]}
+        start={{ x: 0, y: 0 }}
+        end={{ x: 1, y: 1 }}
+        style={styles.background}
+      >
+        <Image
+          source={require("../../assets/images/churchlogo.png")}
+          style={styles.logo}
+          resizeMode="contain"
+        />
+
+        <View style={styles.headerContainer}>
+          <Text style={styles.title}>OIKIA</Text>
+          <View>
+            <Text style={styles.title2}>CHRISTIAN</Text>
+            <Text style={styles.title2}>CENTRE</Text>
+          </View>
+        </View>
+      </LinearGradient>
+    </>
+  );
 }
 
 const styles = StyleSheet.create({
-    content:{
-        flex:1,
-        backgroundColor: 'blue'
-    }
+  background: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 24,
+  },
+  logo: {
+    width: 180,
+    height: 180,
+    marginBottom: 24,
+  },
+  headerContainer: {
+    marginTop: 20,
+    alignSelf: "center",
+    flexDirection: "row",
+    gap: 7,
+    alignItems: "center",
+  },
 
-})
+  title: {
+    fontSize: 40,
+    color: "#FFFFFF",
+    fontWeight: "bold",
+  },
+
+  title2: {
+    fontSize: 18,
+    color: "#FFFFFF",
+    lineHeight: 17,
+  },
+});
--- component/CustomSplashScreens/CustomSplashScreen2.tsx
+++ component/CustomSplashScreens/CustomSplashScreen2.tsx
@@ -1,10 +1,19 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Image, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
 export default function CustomSplashScreen2() {
   return (
-    <View style={styles.content}>
-      <Text>CustomSplashScreen2</Text>
+    <View style={styles.content}> 
+    <Image style={styles.icon} source={require('../../assets/images/churchlogo.png')}/>
+      
+       <View style={styles.headerContainer}>
+              <Text style={styles.title}>OIKIA</Text>
+              <View>
+                <Text style={styles.title2}>CHRISTIAN</Text>
+                <Text style={styles.title2}>CENTRE</Text>
+              </View>
+        </View>
+     
     </View>
   )
 }
@@ -12,6 +21,34 @@ export default function CustomSplashScreen2() {
 const styles = StyleSheet.create({
     content:{
         flex:1,
-        backgroundColor:'blue'
-    }
+        alignItems:'center',
+        justifyContent: 'center',
+        backgroundColor: 'white'
+    },
+
+    icon:{
+        width:174,
+        height:168
+    },
+
+  headerContainer: {
+    marginTop: 20,
+    alignSelf: 'center',
+    flexDirection: 'row',
+    gap: 7,
+    alignItems: 'center',
+  },
+
+  title: {
+    fontSize: 40,
+    color: '#8C4616',
+    fontWeight: 'bold',
+  },
+
+  title2: {
+    fontSize: 18,
+    color: '#8C4616',
+    lineHeight: 17,
+  },
+
 })
