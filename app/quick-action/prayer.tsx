344e5412f31a3a28e0e5b78802fda4e658707732
[joshua nduka] <[ndukajoshua6@gmail.com]>
2026-02-12 19:14:12 +0100

    j

--- app/quick-action/prayer.tsx
+++ app/quick-action/prayer.tsx
@@ -1,4 +1,4 @@
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
+import {  StyleSheet, View } from 'react-native'
 import React from 'react'
 import PrayerSection from '@/component/PrayerSection/PrayerSection'
 
